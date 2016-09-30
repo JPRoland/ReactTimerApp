9a2a642bc30d5214c1e88049e705dcd65caf4b6a
Joe Roland <joroland@gmail.com>
2016-09-30 16:51:26 -0400

    Initial Commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+const React = require('react');
+const {Link, IndexLink} = require('react-router');
+
+const Nav = (props) => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created By <a href="http://github.com/JPRoland" target="_blank">Joe Roland</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Nav;
